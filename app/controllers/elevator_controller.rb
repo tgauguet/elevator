a0611543a2bd7aa311ac7cbba3e6f07074b0a92b
tgauguet <tgauguet@student.42.fr>
2018-04-22 10:39:40 +0200

    added a few styles

--- app/controllers/elevator_controller.rb
+++ app/controllers/elevator_controller.rb
@@ -1,9 +1,7 @@
 class ElevatorController < ApplicationController
 
   def show
-    @elevator = Elevator.last
-    @inside_calls = @elevator.calls.inside.uncomplete
-    @outside_calls = @elevator.calls.outside.uncomplete
+    @elevators = Elevator.all.order("created_at ASC")
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,6 @@ Elevator.delete_all
 Call.delete_all
 
 elevator = Elevator.create(name: "first elevator", floor: 5, position: 0, direction: "up")
+elevator = Elevator.create(name: "second elevator", floor: 3, position: 0, direction: "up")
 
 Call.create(elevator_id: elevator.id, direction: "up", floor_request: 3)
